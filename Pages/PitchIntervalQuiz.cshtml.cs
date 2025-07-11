8c5c7522b96f6d8dbf21d20f23924cfa60120281
Guillaume Lacasse <glacasse@gmail.com>
2025-07-11 17:09:55 -0400

    fix octave

--- Pages/PitchIntervalQuiz.cshtml.cs
+++ Pages/PitchIntervalQuiz.cshtml.cs
@@ -1,27 +1,27 @@
 using ChordProgressionQuiz.Models;
 using ChordProgressionQuiz.Services;
+using Microsoft.AspNetCore.Http; // Required for Session
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
+using System.Text.Json; // Required for JSON serialization
 
 namespace ChordProgressionQuiz.Pages
 {
     public class PitchIntervalQuizModel : PageModel
     {
         private readonly PitchIntervalService _intervalService;
-        private readonly Random _random = new Random();
 
         public PitchInterval CurrentInterval { get; set; }
-        public string AllIntervalsJson { get; set; } // MODIFIED: Will hold all possible intervals
+        public string AllIntervalsJson { get; set; }
         public List<string> AllIntervalNames { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public bool LoopPlayback { get; set; } = true;
 
-        [BindProperty(SupportsGet = true)] // NEW: Property for the toggle
+        [BindProperty(SupportsGet = true)]
         public bool PrioritizeWeaknesses { get; set; } = false;
 
 
@@ -30,24 +30,40 @@ namespace ChordProgressionQuiz.Pages
             _intervalService = intervalService;
         }
 
-        // MODIFIED: OnGet now handles selecting a specific interval or a random one
+        // MODIFIED: This method now uses the session to maintain a stable list of intervals
         public void OnGet(int? selectedIndex)
         {
-            var allPossibleIntervals = _intervalService.GetAllPossibleIntervals();
+            string sessionIntervalsKey = "PitchIntervalQuiz_Intervals";
+            List<PitchInterval> allPossibleIntervals;
+            var intervalsJsonFromSession = HttpContext.Session.GetString(sessionIntervalsKey);
+
+            if (string.IsNullOrEmpty(intervalsJsonFromSession))
+            {
+                // If no list exists in the session, generate a new randomized list...
+                allPossibleIntervals = _intervalService.GetAllPossibleIntervals();
+                var jsonToStore = JsonSerializer.Serialize(allPossibleIntervals);
+                // ...and store it in the session for future requests.
+                HttpContext.Session.SetString(sessionIntervalsKey, jsonToStore);
+            }
+            else
+            {
+                // If a list already exists, retrieve that stable list from the session.
+                allPossibleIntervals = JsonSerializer.Deserialize<List<PitchInterval>>(intervalsJsonFromSession);
+            }
+
             AllIntervalsJson = JsonSerializer.Serialize(allPossibleIntervals);
             AllIntervalNames = _intervalService.GetAllIntervalNames();
 
             if (selectedIndex.HasValue && selectedIndex.Value >= 0 && selectedIndex.Value < allPossibleIntervals.Count)
             {
+                // Select the specific interval the user chose
                 CurrentInterval = allPossibleIntervals[selectedIndex.Value];
             }
             else
             {
-                // Default to a completely random one for the very first load
+                // For the very first visit, get a single, truly random interval
                 CurrentInterval = _intervalService.GetRandomInterval();
             }
         }
-
-        // The OnPostNext handler is no longer needed, as the "Next" button will be handled by JavaScript.
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,3 @@
-// Startup.cs
 using ChordProgprogressionQuiz.Services;
 using ChordProgressionQuiz.Services;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System; // Required for TimeSpan
 
 namespace ChordProgressionQuiz
 {
@@ -19,20 +19,26 @@ namespace ChordProgressionQuiz
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
 
-            // Register the ChordProgressionService as a Singleton.
+            // Register Services
             services.AddSingleton<ChordProgressionService>();
-            // Register the ChordStylingService as a Singleton.
             services.AddSingleton<ChordStylingService>();
-            // NEW: Register the PitchIntervalService as a Singleton.
             services.AddSingleton<PitchIntervalService>();
+
+            // NEW: Add services for session state
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(20); // Set a session timeout
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            services.AddHttpContextAccessor(); // Allows access to session from services
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -47,14 +53,15 @@ namespace ChordProgressionQuiz
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
+
+            // NEW: Add this to enable session middleware
+            app.UseSession();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                // Test endpoint (optional, can be removed once confirmed working)
                 endpoints.MapGet("/hello", async context =>
                 {
                     await context.Response.WriteAsync("Hello from Kestrel!");
